887cae7a9678298faad0cbe158a41209bf5f7abc
Beezwax <kolomiec.evgeniy03@gmail.com>
2025-07-26 14:39:17 +0300

    fix: cors

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,11 +1,9 @@
 import { IsEmail, IsNotEmpty } from 'class-validator'
 
 export class LoginDto {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsEmail()
   email: string
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsNotEmpty()
   password: string
 }
